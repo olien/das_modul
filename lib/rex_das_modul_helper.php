a0a722e482cb796fec82c9b7dd6810563d1bde1d
Oliver Kreischer <github@kreischer.de>
2018-09-14 15:10:50 +0200

    ..

--- lib/rex_das_modul_helper.php
+++ lib/rex_das_modul_helper.php
@@ -55,6 +55,7 @@ class rex_das_modul_helper
     ////////////////////////////////////
     function container_input($id)
     {
+        /*
         $s = new rex_select();
         $s->setName('REX_INPUT_VALUE['.$id.'][0][container]');
         $s->addOptions([
@@ -81,7 +82,7 @@ class rex_das_modul_helper
         ';
 
         echo $form;
-          /*
+        */
         $mform = new MForm();
         $mform->addFieldset('Breite des Inhaltes <i class="module_help_link fa fa-exclamation-triangle" aria-hidden="true"></i>');
         $mform->addHtml('<div class="module_help_content">
@@ -93,7 +94,7 @@ class rex_das_modul_helper
             'container-fluid'  => 'volle Browserbreite'
         ), array('label' => 'Breite'));
         echo $mform->show();
-          */
+     
      }
 
     ////////////////////////////////////
